51c45e145e7165937a1dab4462bd92d765de2e8b
jebarcha <jebarcha@hotmail.com>
2020-08-13 16:55:33 -0500

    graficas agregadas

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-grafica1',
-  templateUrl: './grafica1.component.html',
-  styles: [
-  ]
+	selector: 'app-grafica1',
+	templateUrl: './grafica1.component.html',
+	styles: []
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component {
+	labels1: string[] = [ 'Alitas', 'Papas', 'Refresco' ];
+	labels2: string[] = [ 'Carne', 'Carbon', 'Cerveza' ];
+	labels3: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+	labels4: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	public data1 = [ [ 50, 15, 5 ] ];
+	public data2 = [ [ 10, 50, 90 ] ];
+	public data3 = [ [ 350, 450, 100 ] ];
+	public data4 = [ [ 100, 20, 15 ] ];
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+// Modulos
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -10,7 +14,7 @@ import { PagesComponent } from './pages.component';
 
 @NgModule({
 	declarations: [ DashboardComponent, ProgressComponent, Grafica1Component, PagesComponent ],
-	imports: [ CommonModule, SharedModule, RouterModule ],
+	imports: [ CommonModule, FormsModule, SharedModule, RouterModule, ComponentsModule ],
 	exports: [ DashboardComponent, ProgressComponent, Grafica1Component, PagesComponent ]
 })
 export class PagesModule {}
