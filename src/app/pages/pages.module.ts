09462836bd4467c314feedc76187cec2d7805841
jebarcha <jebarcha@hotmail.com>
2020-08-13 22:02:39 -0500

    Servicio y menu dinamico

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -11,9 +11,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
-	declarations: [ DashboardComponent, ProgressComponent, Grafica1Component, PagesComponent ],
+	declarations: [ DashboardComponent, ProgressComponent, Grafica1Component, PagesComponent, AccountSettingsComponent ],
 	imports: [ CommonModule, FormsModule, SharedModule, RouterModule, ComponentsModule ],
 	exports: [ DashboardComponent, ProgressComponent, Grafica1Component, PagesComponent ]
 })
---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class SidebarService {
+	menu: any[] = [
+		{
+			titulo: 'Dashboard',
+			icono: 'mdi mdi-gauge',
+			submenu: [
+				{ titulo: 'Main', url: '/' },
+				{ titulo: 'Progressbar', url: '/dashboard/progress' },
+				{ titulo: 'Graficas', url: '/dashboard/grafica1' }
+			]
+		}
+	];
+
+	constructor() {}
+}
