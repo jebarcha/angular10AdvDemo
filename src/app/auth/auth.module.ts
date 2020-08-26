c8440a08dc77292ec0e9272041ad98e0d963494b
jebarcha <jebarcha@hotmail.com>
2020-08-26 11:02:58 -0500

    Login and Auth complete

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [ LoginComponent, RegisterComponent ],
-	imports: [ CommonModule, RouterModule, FormsModule ],
+	imports: [ CommonModule, RouterModule, FormsModule, ReactiveFormsModule, HttpClientModule ],
 	exports: [ LoginComponent, RegisterComponent ]
 })
 export class AuthModule {}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import Swal from 'sweetalert2';
+
+import { UsuarioService } from '../../services/usuario.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-register',
 	templateUrl: './register.component.html',
 	styleUrls: [ './register.component.css' ]
 })
-export class RegisterComponent implements OnInit {
-	constructor() {}
+export class RegisterComponent {
+	public formSubmitted = false;
 
-	ngOnInit(): void {}
+	public registerForm = this.fb.group(
+		{
+			nombre: [ 'Ringo', [ Validators.required, Validators.minLength(3) ] ],
+			email: [ 'ringo2@gmail.com', [ Validators.required, Validators.email ] ],
+			password: [ '', Validators.required ],
+			password2: [ '', Validators.required ],
+			terminos: [ false, Validators.required ]
+		},
+		{
+			validators: this.passwordsIguales('password', 'password2')
+		}
+	);
+
+	constructor(private fb: FormBuilder, private usuarioService: UsuarioService, private router: Router) {}
+
+	crearUsuario() {
+		this.formSubmitted = true;
+		console.log(this.registerForm.value);
+
+		if (this.registerForm.invalid) {
+			return;
+		}
+
+		this.usuarioService.crearUsuario(this.registerForm.value).subscribe(
+			(resp) => {
+				// Navegar al Dashboard
+				this.router.navigateByUrl('/');
+			},
+			(err) => {
+				// Si sucede un error
+				Swal.fire('Error', err.error.msg, 'error');
+			}
+		);
+	}
+
+	campoNoValido(campo: string): boolean {
+		if (this.registerForm.get(campo).invalid && this.formSubmitted) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	contrasenasNoValidas() {
+		const pass1 = this.registerForm.get('password').value;
+		const pass2 = this.registerForm.get('password2').value;
+
+		if (pass1 !== pass2 && this.formSubmitted) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	aceptaTerminos() {
+		return !this.registerForm.get('terminos').value && this.formSubmitted;
+	}
+
+	passwordsIguales(pass1Name: string, pass2Name: string) {
+		return (formGroup: FormGroup) => {
+			const pass1Control = formGroup.get(pass1Name);
+			const pass2Control = formGroup.get(pass2Name);
+
+			if (pass1Control.value === pass2Control.value) {
+				pass2Control.setErrors(null);
+			} else {
+				pass2Control.setErrors({ noEsIgual: true });
+			}
+		};
+	}
 }
